0c842a525c6f740c0765871d7bf153360bd7fd73
laicheehou <34847925+laicheehou@users.noreply.github.com>
2022-07-14 15:33:12 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,4 +12,4 @@ def root():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
